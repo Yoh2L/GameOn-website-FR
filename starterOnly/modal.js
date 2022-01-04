a53edb4f66a4b467f557cfb59197cae76a21d104
Yohan <yohan.deletoille@gmail.com>
2022-01-04 17:20:22 +0100

    V0.2 - Premieres validations des formulaires

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -12,6 +12,8 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const closeModal = document.querySelector(".close");
+const inputsTxt = document.querySelectorAll('input[type="text"], input[type="email"], input[type="date"], input[type="number"]');
+const city = document.querySelectorAll("input[type=radio]");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -25,5 +27,131 @@ function launchModal() {
 closeModal.addEventListener("click", () => {
   modalbg.style.display = "none";
 });
+
+// name / email / date / quantity checker
+
+let first, last, email, date, quantity, checkboxes, cgv;
+
+const errorDisplay = (tag, message, valid) =>{
+  const span = document.querySelector("." + tag + "-container > span");
+  const div= document.querySelector("." + tag + "-container > input");
+
+  if(!valid) {
+    div.classList.add("error");
+    div.classList.remove("no-error");
+    span.textContent = message;
+  } else {
+    div.classList.add("no-error");
+    div.classList.remove("error");
+    span.textContent = message;
+  }
+}
+
+const fnameChecker = (value) => {
+  if (value.length < 2 || value.length > 20){
+    errorDisplay("first", "Le prénom doit être compris entre 2 et 20 caractères.")
+    first = null;
+  }
+  else if (!value.match(/^[a-zA-Z\u00C0-\u017F]*$/)){
+    errorDisplay("first", "Le prénom ne doit pas contenir de caractères spéciaux.")
+    first = null;
+  }
+  else {
+    errorDisplay("first", "", true);
+    first = value;
+  }
+
+};
+
+const lnameChecker = (value) => {
+  if (value.length < 2 || value.length > 20){
+    errorDisplay("last", "Le nom doit être compris entre 2 et 20 caractères.")
+    last = null;
+  }
+  else if (!value.match(/^[a-zA-Z\u00C0-\u017F]*$/)){
+    errorDisplay("last", "Le nom ne doit pas contenir de caractères spéciaux.")
+    last = null;
+  }
+  else {
+    errorDisplay("last", "", true);
+    last = value;
+  }
+};
+
+const emailChecker = (value) => {
+  if (!value.match(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/i)) {
+    errorDisplay('email', "Le mail n'est pas valide.");
+    email = null;
+  }
+  else {
+    errorDisplay("email", "", true);
+    email = value;
+  }
+
+};
+
+const dateChecker = (value) => {
+  if (value.match(/^([0-9]{2})\/([0-9]{2})\/([0-9]{4})$/)) {
+    errorDisplay("birthdate", "", true);
+    date = value;
+
+  }
+  else {
+    errorDisplay("birthdate", "La date de naissance n'est pas valide.")
+    date = null;
+  }
+
+};
+
+
+
+const quantityChecker = (value) => {
+  if (!value.match(/^\d+$/)) {
+    errorDisplay('quantity', "Le nombre de concours n'est pas valide.")
+    quantity = null;
+  }
+  else {
+    errorDisplay('quantity', "", true);
+    quantity = value;
+  }
+
+};
+
+document.addEventListener('submit', function() {
+  var len = document.querySelectorAll('.checkbox input[type="checkbox"]:checked').length;
+  if (len <= 0) {
+    msg.innerHTML = "Please check at least one";
+  } else {
+    msg.innerHTML = "Your have checked <span style='color:red'>" + len + "</span> checkbox";
+  }
+})
+
+
+
+inputsTxt.forEach((input) =>{
+  input.addEventListener("input", (e) => {
+    switch (e.target.id){
+      case "first" :
+        fnameChecker(e.target.value)
+        break;
+      case "last" :
+        lnameChecker(e.target.value)
+        break;
+      case "email" :
+        emailChecker(e.target.value)
+        break;
+      case "birthdate" :
+        dateChecker(e.target.value)
+        break;
+      case "quantity" :
+        quantityChecker(e.target.value)
+        break;
+      default:
+        nul;
+    }
+  })
+})
+
+
 
 
